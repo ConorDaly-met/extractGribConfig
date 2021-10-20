433bbaffa1161dedf8f04adb838d70413babc232
Conor Daly <conor.daly@met.ie>
2021-10-20 12:09:07 +0000

    Conor Daly: Add module load extractGrib

--- bin/makeparamlists.sh
+++ bin/makeparamlists.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # Check for extractGrib scripts
+module load use.own
+module load extractGrib
+module list
 which extractGrib 2> /dev/null
 if [ $? -ne 0 ]; then
     echo "Error: extractGrib not found, please 'module load extractGrib' first" 
     exit 1
 fi
 
+echo "Generating namelist fragments from paramlists..."
 bindir=$(dirname $0)
 wrkdir=${bindir}/../
 
